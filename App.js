b7b206bc82dd6c0d6c5e82be5398bc03596c39e3
zhenwei zhenwei <zhenwei2972@hotmail.com>
2021-06-10 14:52:56 +0800

    finished navigation and passing data from main screen to subscreen

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+import DetailsScreen from "./screens/DetailsScreen";
+
+function HomeScreen({ navigation }) {
+  const [colors, setColors] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
+  ]);
+
+  function renderItem({ item }) {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Details", { ...item });
+        }}
+      >
+        <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+      </TouchableOpacity>
+    );
+  }
+
+  function addColor() {
+    setColors([
+      {
+        red: Math.floor(Math.random() * 255),
+        green: Math.floor(Math.random() * 255),
+        blue: Math.floor(Math.random() * 255),
+        id: `${colors.length}`,
+      },
+      ...colors,
+    ]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={addColor} title="Add Color" />
+      <FlatList
+        style={{ width: "100%" }}
+        data={colors}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eee",
+    alignItems: "center",
   },
 });
---
+++ screens/DetailsScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function DetailsScreen({ route }) {
+  const { red, green, blue } = route.params;
+  return (
+    <View>
+      <Text>Red: {red}</Text>
+      <Text>Green: {green}</Text>
+      <Text>Blue:{blue}</Text>
+    </View>
+  );
+}
